7860301863a0278548ab9a571ae84422ed0a63dd
Imrancu <mdimran3772@gmail.com>
2022-02-10 19:56:06 +0600

    update

--- js/banking.js
+++ js/banking.js
@@ -2,6 +2,25 @@
 document.getElementById('deposit-btn').addEventListener('click', function() {
     // get the amount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+
+    // Update deposit total
+
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositAmountText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+    // Update Account Balance
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceText = totalBalance.innerText;
+    const previousTotalBalance = parseFloat(totalBalanceText);
+    const newTotalBalance = previousTotalBalance + newDepositAmount;
+    totalBalance.innerText = newTotalBalance;
+
+    // clear the deposit input field
+    depositInput.value = '';
 });
