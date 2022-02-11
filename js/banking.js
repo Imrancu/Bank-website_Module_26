81b50f61e3964e7c3d0da2c40058f2d0518d6d19
imrancu <mdimran3772@gmail.com>
2022-02-11 10:40:10 +0600

    done Banking website

--- js/banking.js
+++ js/banking.js
@@ -24,3 +24,29 @@ document.getElementById('deposit-btn').addEventListener('click', function() {
     // clear the deposit input field
     depositInput.value = '';
 });
+
+// Handle withdraw event handler
+document.getElementById('withdraw-btn').addEventListener('click', function() {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // Set withdraw Total
+    const totalWithdraw = document.getElementById('total-withdraw');
+    const previousTotalWithdrawText = totalWithdraw.innerText;
+    const previousTotalWithdraw = parseFloat(previousTotalWithdrawText);
+
+    const newTotalWithdraw = previousTotalWithdraw + newWithdrawAmount;
+    totalWithdraw.innerText = newTotalWithdraw;
+
+    // Update Balance\
+    const totalBalance = document.getElementById('total-balance');
+    const previousBalanceText = totalBalance.innerText;
+    const previousTotalBalance = parseFloat(previousBalanceText);
+    const newTotalBalance = previousTotalBalance - newWithdrawAmount;
+
+    totalBalance.innerText = newTotalBalance;
+
+    // Clear withdraw input
+    withdrawInput.value = '';
+});
